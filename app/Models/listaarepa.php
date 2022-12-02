95bde28b19b532d14052a1e17d9e3f0692a7b84a
jacokduran <117541814+jacokduran@users.noreply.github.com>
2022-12-02 16:18:47 -0500

    Add files via upload

---
+++ app/Models/listaarepa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+//MAARICO NO TE OLVIDES DE ESTO- PARA PODER CORREGIR MAYUSCULAS
+use Illuminate\Database\Eloquent\Casts\Attribute;
+class listaarepa extends Model
+{
+    use HasFactory;
+    protected $table = "arepas";
+
+  
+
+   protected $filtable = [
+       "arepa",
+       "precio", 
+       "descripcion",
+   ]; 
+   //CORREGIR MAYUSCULAS
+   protected function arepa(): Attribute{
+       return new Attribute(
+           get: fn($value) => ucwords($value),
+           set: fn($value) => strtolower($value),
+       ); 
+   }
+}
